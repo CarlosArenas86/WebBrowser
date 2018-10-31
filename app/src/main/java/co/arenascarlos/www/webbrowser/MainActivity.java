a519875fefb12699ca532b8932cd8e0fd868d455
Carlos <carlos.acrif@gmail.com>
2018-11-01 11:20:52 +1300

    Add second Layout with the editText and Button.

--- app/src/main/java/co/arenascarlos/www/webbrowser/MainActivity.java
+++ app/src/main/java/co/arenascarlos/www/webbrowser/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         superProgressBar = findViewById(R.id.myProgressBar);
         superImageView = findViewById(R.id.myImageView);
