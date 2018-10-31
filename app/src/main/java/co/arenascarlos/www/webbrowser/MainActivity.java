419c0f771c3e99c0684691f4c17ccc4c5eafe163
Carlos <carlos.acrif@gmail.com>
2018-10-31 21:43:12 +1300

    Creating The WebView

--- app/src/main/java/co/arenascarlos/www/webbrowser/MainActivity.java
+++ app/src/main/java/co/arenascarlos/www/webbrowser/MainActivity.java
@@ -2,14 +2,23 @@ package co.arenascarlos.www.webbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ProgressBar superProgressBar;
+    private ImageView superImageView;
+    private WebView superWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+     superProgressBar = findViewById(R.id.)
     }
 }
 
-// second commit 
+
